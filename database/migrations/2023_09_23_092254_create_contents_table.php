410ccecc21653d54a1b7890ba4441ec22c965ea8
manansaipi <abdul.saipi@student.president.ac.id>
2023-09-25 13:05:50 +0700

    add envexmaple

--- database/migrations/2023_09_23_092254_create_contents_table.php
+++ database/migrations/2023_09_23_092254_create_contents_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('contents', function (Blueprint $table) {
             $table->id();
-            $table->string('htnl');
+            $table->integer('cluster');
             $table->timestamps();
         });
     }
