8e234a630cc4d55a1e2d44c30ee8e56b189afb02
Faiz Jazadi <me@lcat.dev>
2023-11-13 14:53:49 +0700

    Implement change password feature
    Signed-off-by: Faiz Jazadi <me@lcat.dev>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function show()
     {
         $user = auth()->user();
 
@@ -19,51 +17,18 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function update(Request $request)
     {
-        //
-    }
+        $user = auth()->user();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        $data = $request->validate([
+            'password' => ['required', 'string', 'min:3', 'confirmed'],
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $user)
-    {
-        //
-    }
+        User::where('id', $user->id)->update([
+            'password' => Hash::make($data['password'])
+        ]);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user)
-    {
-        //
+        return redirect('/profile')->with('success', 'Profile updated!');
     }
 }
--- resources/views/dashboard/contents/view-profile.blade.php
+++ resources/views/dashboard/contents/view-profile.blade.php
@@ -9,77 +9,100 @@
 <div class="main main-app p-3 p-lg-4">
     <h1 class="mb-3">Profil</h1>
 
+    @if (session()->has('success'))
+    <div class="alert alert-success">
+        <p>{{ session('success') }}</p>
+    </div>
+    @endif
+
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+
     <div class="row g-3">
-        <div class="card card-settings">
-            <div class="card-body p-0">
+        <form method="post" action="">
+            @csrf
+            <div class="card card-settings">
+                <div class="card-body p-0">
 
-                <div class="setting-item">
-                    <div class="row g-2 align-items-center">
-                        <div class="col-md-5">
-                            <h6>Foto Profil</h6>
-                            <p>Gunakan foto profil yang menampilkan wajah Anda dengan jelas</p>
-                        </div><!-- col -->
-                        <div class="col-md">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/1024px-Windows_10_Default_Profile_Picture.svg.png" style="height: 3rem" alt="..." class="mx-2">
-                            <input type="file" id="newProfilePicInput" class="form-control d-none mw-10" placeholder="Masukkan foto profil Anda">
-                            <button onclick="$('#newProfilePicInput').click()" class="btn btn-white">Upload foto baru</button>
-                            <!-- TODO: handle selected file properly and display the preview -->
-                        </div><!-- col -->
-                    </div><!-- row -->
-                </div><!-- setting-item -->
+                    <div class="setting-item">
+                        <div class="row g-2 align-items-center">
+                            <div class="col-md-5">
+                                <h6>Foto Profil</h6>
+                                <p>Gunakan foto profil yang menampilkan wajah Anda dengan jelas</p>
+                            </div><!-- col -->
+                            <div class="col-md">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/1024px-Windows_10_Default_Profile_Picture.svg.png" style="height: 3rem" alt="..." class="mx-2">
+                                <input type="file" id="newProfilePicInput" class="form-control d-none mw-10" placeholder="Masukkan foto profil Anda">
+                                <button onclick="$('#newProfilePicInput').click()" type="button" class="btn btn-white">Upload foto baru</button>
+                                <!-- TODO: handle selected file properly and display the preview -->
+                            </div><!-- col -->
+                        </div><!-- row -->
+                    </div><!-- setting-item -->
 
-                <div class="setting-item">
-                    <div class="row g-2 align-items-center">
-                        <div class="col-md-5">
-                            <h6>Nama</h6>
-                            <p>Nama pengguna Anda yang ditampilkan</p>
-                        </div><!-- col -->
-                        <div class="col-md">
-                            <input type="text" class="form-control" placeholder="Masukkan nama Anda" value="{{ $user->name }}" readonly>
-                        </div><!-- col -->
-                    </div><!-- row -->
-                </div><!-- setting-item -->
+                    <div class="setting-item">
+                        <div class="row g-2 align-items-center">
+                            <div class="col-md-5">
+                                <h6>Nama</h6>
+                                <p>Nama pengguna Anda yang ditampilkan</p>
+                            </div><!-- col -->
+                            <div class="col-md">
+                                <input type="text" class="form-control" placeholder="Masukkan nama Anda" value="{{ $user->name }}" readonly>
+                            </div><!-- col -->
+                        </div><!-- row -->
+                    </div><!-- setting-item -->
 
-                <div class="setting-item">
-                    <div class="row g-2 align-items-center">
-                        <div class="col-md-5">
-                            <h6>Email</h6>
-                            <p>Email yang terdaftar</p>
-                        </div><!-- col -->
-                        <div class="col-md">
-                            <input type="text" class="form-control" placeholder="Masukkan email Anda" value="{{ $user->email }}" readonly>
-                        </div><!-- col -->
-                    </div><!-- row -->
-                </div><!-- setting-item -->
+                    <div class="setting-item">
+                        <div class="row g-2 align-items-center">
+                            <div class="col-md-5">
+                                <h6>Email</h6>
+                                <p>Email yang terdaftar</p>
+                            </div><!-- col -->
+                            <div class="col-md">
+                                <input type="text" class="form-control" placeholder="Masukkan email Anda" value="{{ $user->email }}" readonly>
+                            </div><!-- col -->
+                        </div><!-- row -->
+                    </div><!-- setting-item -->
 
-                <div class="setting-item">
-                    <div class="row g-2 align-items-center">
-                        <div class="col-md-5">
-                            <h6>Password baru</h6>
-                            <p>Gunakan password yang aman dan sulit untuk ditebak</p>
-                        </div><!-- col -->
-                        <div class="col-md">
-                            <input type="password" class="form-control" placeholder="Masukkan password baru">
-                        </div><!-- col -->
-                    </div><!-- row -->
-                </div><!-- setting-item -->
+                    <div class="setting-item">
+                        <div class="row g-2 align-items-center">
+                            <div class="col-md-5">
+                                <h6>Password baru</h6>
+                                <p>Gunakan password yang aman dan sulit untuk ditebak</p>
+                            </div><!-- col -->
+                            <div class="col-md">
+                                <input type="password" name="password" class="form-control" placeholder="Masukkan password baru">
+                            </div><!-- col -->
+                        </div><!-- row -->
+                    </div><!-- setting-item -->
 
-                <div class="setting-item">
-                    <div class="row g-2 align-items-center">
-                        <div class="col-md-5">
-                            <h6>Konfirmasi Password Baru</h6>
-                            <p>Ketik ulang password sesuai dengan sebelumnya</p>
-                        </div><!-- col -->
-                        <div class="col-md">
-                            <input type="password" class="form-control" placeholder="Masukkan password baru">
-                        </div><!-- col -->
-                    </div><!-- row -->
-                </div><!-- setting-item -->
+                    <div class="setting-item">
+                        <div class="row g-2 align-items-center">
+                            <div class="col-md-5">
+                                <h6>Konfirmasi Password Baru</h6>
+                                <p>Ketik ulang password sesuai dengan sebelumnya</p>
+                            </div><!-- col -->
+                            <div class="col-md">
+                                <input type="password" name="password_confirmation" class="form-control" placeholder="Masukkan password baru">
+                            </div><!-- col -->
+                        </div><!-- row -->
+                    </div><!-- setting-item -->
 
-            </div><!-- card-body -->
-        </div><!-- card -->
+                </div><!-- card-body -->
+            </div><!-- card -->
 
-        <button class="btn btn-primary btn-sign">Simpan Profil</button>
+            <button type="submit" class="btn mt-3 btn-primary btn-sign">Simpan Profil</button>
+
+        </form>
+
     </div>
 
     <div class="main-footer mt-5">
